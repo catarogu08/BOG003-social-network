5ee7725831b88a7d74792da10a5a07259ef91d45
Catarogu08 <cataguatame2963@gmail.com>
2021-09-28 11:51:20 -0500

    primeros pasos registrarse

--- src/pantallas/Function/main.js
+++ src/pantallas/Function/main.js
@@ -1,5 +1,24 @@
-// Este es el punto de entrada de tu aplicacion
+//import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+  
+  const register = document.getElementById("logIn");
+    
 
-import { myFunction } from './lib/index.js';
+   register.addEventListener('click', (e) =>{
+    const createPassword = document.getElementById("password").value;
+    const createEmail = document.getElementById("email").value;
+    const user = document.getElementById("yourName").value;
+    const auth = getAuth(createEmail, createPassword, user)
+    createUserWithEmailAndPassword(auth, email, user, password)
+  .then((userCredential) => {
+    // Signed in
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+  });
 
-myFunction();
+     
+});
