4f4da8687d0ab6020a68b4dbaf00333dd5d4e3e4
Catarogu08 <cataguatame2963@gmail.com>
2021-10-27 09:19:18 -0500

    CRUD Firestore

--- src/lib/views/templateHome.js
+++ src/lib/views/templateHome.js
@@ -31,11 +31,11 @@ export const home = () => {
   <h3>Marca:</h3>
   <input type="text" id="mark">
   <h3>Modelo:</h3>
-  <input type="number" id="model">
+  <input type="text" id="model">
   <h3>Descrpicion:</h3>
   <input type="text" id="description">
   <button type="submit" id="creating">Crear</button>
-  <button>Subir Cambios</button>
+  <button type="submit" id="newElement">Subir Cambios</button> 
      </form>
       <a href="#/home"></a>
     </section>
@@ -58,81 +58,130 @@ export const home = () => {
     </div>
     </footer>
     `
-    divHome.innerHTML = viewHome;
-    
+  divHome.innerHTML = viewHome;
 
-//initialize Firestore
+
+  //Initialize Firestore
   const db = firebase.firestore();
+
+  //Crear Post
   const btnCreate = divHome.querySelector("#creating");
   btnCreate.addEventListener('click', (e) => {
     const mark = divHome.querySelector("#mark").value;
     const model = divHome.querySelector("#model").value;
     const description = divHome.querySelector("#description").value;
-    if (mark !== '' && model !== '' && description !== '')  {
+    if (mark !== '' && model !== '' && description !== '') {
       console.log('esta completo')
       db.collection("posts").add({
         mark: mark,
-        model:model,
+        model: model,
         description: description,
-       })
-       .then((docRef) => {
-         console.log("Document written with ID: ", docRef.id);
-         divHome.querySelector("#mark").value = '';
-         divHome.querySelector("#model").value = '';
-         divHome.querySelector("#description").value = '';
-       })
-       .catch((error) => {
-         console.error("Error adding document: ", error);
-         window.alert(`ocurrio un error revisa tu form`);
-       });
+      })
+        .then((docRef) => {
+          console.log("Document written with ID: ", docRef.id);
+          divHome.querySelector("#mark").value = '';
+          divHome.querySelector("#model").value = '';
+          divHome.querySelector("#description").value = '';
+        })
+        .catch((error) => {
+          console.error("Error adding document: ", error);
+          window.alert(`ocurrio un error revisa tu form`);
+        });
 
     } else {
       window.alert(`revisa tu informacion`);
     }
   })
-//
- const viewPost = divHome.querySelector('.viewPost')
+
+
+  //Leer Post
+
+  const viewPost = divHome.querySelector('.viewPost')
   db.collection("posts").onSnapshot((querySnapshot) => {
     viewPost.innerHTML = '';
     querySnapshot.forEach((doc) => {
       console.log(`${doc.id} => ${doc.data()}`);
       viewPost.innerHTML += `
       <div class="post">
-      <div>2
-        <p id='marked-${doc.id}'>Marca: ${doc.data().mark} </p>
+      <div>
+        <p id='mark-${doc.id}'>Marca: ${doc.data().mark} </p>
       </div>
       <div>
-        <p>Modelo:  ${doc.data().model}</p>
+        <p id='model-${doc.id}'>Modelo: ${doc.data().model} </p>
       </div>
       <div>
-        <p>Descripcion: ${doc.data().description}</p>
+        <p  id='description-${doc.id}'> Descripcion: ${doc.data().description} </p>
       </div>
       <div>
         <div class="btnOption">
-          <button class="btnDeleted"> Eliminar </button>
-          <button class="btnEdit" data-docid="${doc.id}"> Editar </button>
+          <button class="btnDeleted" data-id=${doc.id}> Eliminar </button>
+          <button class="btnEdit" data-id=${doc.id}> Editar </button> 
         </div>
-        <div class="like">
-          <button id="btnLike"><img src="./img/like.png" width="55px" alt=""></button>
-        </div>
-      </div>
     </div>
     `
+      //envio parametros atravez de un string 104-123
+    });
+
+
+    //Eliminar post
+    let btnDeleted = divHome.querySelectorAll('.btnDeleted');
+    btnDeleted.forEach((btnD) => {
+      btnD.addEventListener('click', (e) => {
+        const id = e.target.dataset.id;
+        db.collection("posts").doc(id).delete()
+          .then(() => {
+            console.log("Document successfully deleted!");
+          }).catch((error) => {
+            console.error("Error removing document: ", error);
+          });
+      })
+    })
+
+
+
+    //Editar Post
+    let btnEditer = divHome.querySelectorAll('.btnEdit');
+    btnEditer.forEach((btn) => {
+      btn.addEventListener('click', (e) => {
+        const button = e.target;
+        const id = e.target.dataset.id;
+        console.log(id)
+        const valorMark = divHome.querySelector(`#mark-${id}`).textContent;
+        const valorModel = divHome.querySelector(`#model-${id}`).textContent;
+        const valorDescription = divHome.querySelector(`#description-${id}`).textContent;
+        console.log(valorMark, valorModel, valorDescription);
+
+        divHome.querySelector("#mark").value = valorMark;      // que mark, model, descrip viajen al input cn id
+        divHome.querySelector("#model").value = valorModel;
+        divHome.querySelector("#description").value = valorDescription;
+        
+        const btn = divHome.querySelector("#newElement");
+        const btnBlock = btn.style.display = 'block';
+        btn.addEventListener('click', (e) => {
+          console.log('voy a editar')
+          let updatePost = db.collection("posts").doc(id);
+          const marking = divHome.querySelector("#mark").value;
+          const modeling = divHome.querySelector("#model").value;
+          const descripting = divHome.querySelector("#description").value;
+          return updatePost.update({
+            mark: marking,
+            model: modeling,
+            description: descripting,
+          })
+            .then(() => {
+              console.log("Document successfully updated!");
+              divHome.querySelector("#mark").value = '';
+              divHome.querySelector("#model").value = '';
+              divHome.querySelector("#description").value = '';
+
+            })
+            .catch((error) => {
+              console.error("Error updating document: ", error);
+              window.alert(`ocurrio un error al actualizar tu post`);
+            });
+        });
+      });
     });
   });
-
-
-   /* const btnEdits = divHome.querySelectorAll('.btnEdit');
-    btnEdits.forEach( (button) => {
-    button.addEventListener('click', (e) =>{
-      const button = e.target;
-      const id = button.dataset.docid;
-      console.log(id)
-
-    const valorMark = divHome.querySelector(`#marked-${id}`).textContent;
-    console.log(valorMark);
-    })
-    })*/
-
- return divHome;
+  return divHome;
 }
